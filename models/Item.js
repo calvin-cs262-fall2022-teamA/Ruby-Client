a720027c98e1933d1817cb6a9a192867db343e65
agritter <origamifinch@gmail.com>
2022-09-30 15:48:15 -0400

    Change Item constructor

--- models/Item.js
+++ models/Item.js
@@ -2,17 +2,19 @@
   A class representing a certain item in stock (e.g. cups)
 */
 export class Item {
-  constructor(name, amount, minimumAmount, defaultIncrement) {
+  //Create an Item.
+  //item should be a JS object with the same properties as Item.
+  constructor(item) {
     // What the item is called
-    this.name = name;
+    this.name = item.name;
 
     // How much of the item is currently in stock
-    this.amount = amount;
+    this.amount = item.amount;
 
     // The amount at which a low inventory notification should be sent
-    this.minimumAmount = minimumAmount;
+    this.minimumAmount = item.minimumAmount;
 
     // The default number of items added or removed at one time
-    this.defaultIncrement = defaultIncrement;
+    this.defaultIncrement = item.defaultIncrement;
   }
 }
