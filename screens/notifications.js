82ea23f3a69def5ca625f0df636c36a3cfe7e97b
aez9 <aez9@calvin.edu>
2022-11-09 21:55:54 -0500

    Update notifications.js

--- screens/notifications.js
+++ screens/notifications.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, FlatList, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 export default function Notifications({navigation, route}) {
 
 
   return (
-    <View>
-      <Text>This will house the notifications for item quantity alerts.</Text>
+    <View style={notifStyles.notifPage}>
+      <View style={notifStyles.notifContainer}>
+        <Icon name="priority-high" size={35} style={notifStyles.notifIcon}></Icon>
+        <Text style={notifStyles.notifText}>This will house the notifications for item quantity alerts.</Text>
+      </View>
+      
       
     </View>
   );
 }
+
+const notifStyles = StyleSheet.create({
+  notifPage: {
+    justifyContent: 'center'
+  },
+  notifContainer: {
+    borderColor: "red",
+    borderWidth: 5,
+
+    flexDirection: 'row',
+    width: '85%',
+    marginLeft: '7.5%',
+    borderRadius: 25,
+    margin: 10,
+
+  },
+  notifIcon: {
+    padding: 5,
+    alignSelf: 'center',
+    
+  },
+  notifText: {
+    padding: 5,
+    
+  }
+
+
+})
