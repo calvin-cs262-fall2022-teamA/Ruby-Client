1084d95cc49c6b24e03c39da93fae64b01c1e858
aez9 <aez9@calvin.edu>
2022-11-09 23:24:04 -0500

    Update notifications.js

--- screens/notifications.js
+++ screens/notifications.js
@@ -8,11 +8,13 @@ export default function Notifications({navigation, route}) {
 
   return (
     <View style={notifStyles.notifPage}>
-      <View style={notifStyles.notifContainer}>
+      <View style={notifStyles.notifObj}>
         <Icon name="priority-high" size={35} style={notifStyles.notifIcon}></Icon>
-        <Text style={notifStyles.notifText}>This will house the notifications for item quantity alerts.</Text>
-      </View>
       
+        <View style={notifStyles.notifContainer}>
+           <Text style={notifStyles.notifText}>The intentory of an Item is too low. Restock Soon.</Text>
+        </View>
+      </View>
       
     </View>
   );
@@ -23,23 +25,45 @@ const notifStyles = StyleSheet.create({
     justifyContent: 'center'
   },
   notifContainer: {
-    borderColor: "red",
+    borderColor: "rgb(37,65,81)",
     borderWidth: 5,
+    backgroundColor: 'rgb(37,65,81)',
 
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 270,
+    borderRadius: 25,
+    marginLeft: 0,
+    marginRight: 0,
+    marginVertical: 0, 
+
+  },
+  notifObj: {
+    borderColor: "rgb(37,65,81)",
+    borderWidth: 5,
+    backgroundColor: 'grey',
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     width: '85%',
     marginLeft: '7.5%',
-    borderRadius: 25,
+    borderRadius: 30,
     margin: 10,
 
   },
   notifIcon: {
     padding: 5,
+    marginLeft: 10,
     alignSelf: 'center',
+    backgroundColor: 'rgb(255, 0, 102)',
+    borderRadius: 25,
     
   },
   notifText: {
     padding: 5,
+    marginLeft:"2%",
+    color: 'white',
+    fontSize: 15,
     
   }
 
