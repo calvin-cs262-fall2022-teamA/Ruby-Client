a5dbe4c0b6d24eb8bc921d7a22bcd9e1cd791e71
Matthew Walstra <matthew.walstra@gmail.com>
2022-11-11 15:50:05 -0500

    Formatted Screens/Login for Lab11

--- screens/login/splash.js
+++ screens/login/splash.js
@@ -3,6 +3,10 @@ import { View, Text } from 'react-native';
 
 // import { globalStyles } from '../styles/global';
 
+/**
+ * 
+ * @returns Component with loading text
+ */
 export default function SplashScreen() {
   return (
     <View>
