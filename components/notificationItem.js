b21a2fcf8a35cd1035867f67a7db45654116f086
aez9 <aez9@calvin.edu>
2022-11-13 12:25:14 -0500

    aez

---
+++ components/notificationItem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+/* A component used to display each item in the ItemsScreen */
+export default function NotificationItem({ item, navigation }) {
+
+
+
+    return (
+        <View style={NotificationItemStyles.notifObj}>
+            <Icon name="priority-high" size={35} style={NotificationItemStyles.notifIcon}></Icon>
+            <View style={NotificationItemStyles.notifContainer}>
+                <Text style={NotificationItemStyles.notifText}>The intentory of {item} is {item.amount}. Restock Soon.</Text>
+            </View>
+        </View>
+    );
+}
+
+const NotificationItemStyles = StyleSheet.create({
+    notifContainer: {
+        borderColor: "rgb(37,65,81)",
+        borderWidth: 5,
+        backgroundColor: 'rgb(37,65,81)',
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 270,
+        borderRadius: 25,
+        marginLeft: 0,
+        marginRight: 0,
+        marginVertical: 0,
+
+    },
+    notifObj: {
+        borderColor: "rgb(37,65,81)",
+        borderWidth: 5,
+        backgroundColor: 'grey',
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '85%',
+        marginLeft: '7.5%',
+        borderRadius: 30,
+        margin: 10,
+
+    },
+    notifIcon: {
+        padding: 5,
+        marginLeft: 10,
+        alignSelf: 'center',
+        backgroundColor: 'rgb(255, 0, 102)',
+        borderRadius: 25,
+
+    },
+    notifText: {
+        padding: 5,
+        marginLeft: "2%",
+        color: 'white',
+        fontSize: 15,
+
+    }
+});
--- screens/notifications.js
+++ screens/notifications.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import { View, Button, FlatList, StyleSheet, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import { itemsContext } from '../states/itemscontext';
+import { NotificationItem } from '../components/notificationItem';
 
 
 export default function Notifications({ navigation, route }) {
 
-  const noitifyItem = [];
+  const notifyItem = [];
   const { items } = React.useContext(itemsContext);
 
   for (let item of items) {
     if (item.amount <= item.minimumAmount) {
-      noitifyItem.push(item);
+      notifyItem.push(item);
     }
   }
 
 
   return (
     <View style={notifStyles.notifPage}>
-      <View style={notifStyles.notifObj}>
-        <Icon name="priority-high" size={35} style={notifStyles.notifIcon}></Icon>
-
-        <View style={notifStyles.notifContainer}>
-          <Text style={notifStyles.notifText}>The intentory of an Item is too low. Restock Soon.</Text>
-        </View>
-      </View>
-
+      <FlatList data={notifyItem.pop(item)}
+        keyExtractor={(item) => `${item.name}:${item.amount}`} // TODO: also ID
+        renderItem={({ item }) => (
+          <NotificationItem item={item} navigation={navigation}></NotificationItem>
+        )}>
+      </FlatList>
     </View>
   );
 }
@@ -34,48 +32,5 @@ const notifStyles = StyleSheet.create({
   notifPage: {
     justifyContent: 'center'
   },
-  notifContainer: {
-    borderColor: "rgb(37,65,81)",
-    borderWidth: 5,
-    backgroundColor: 'rgb(37,65,81)',
-
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: 270,
-    borderRadius: 25,
-    marginLeft: 0,
-    marginRight: 0,
-    marginVertical: 0,
-
-  },
-  notifObj: {
-    borderColor: "rgb(37,65,81)",
-    borderWidth: 5,
-    backgroundColor: 'grey',
-
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '85%',
-    marginLeft: '7.5%',
-    borderRadius: 30,
-    margin: 10,
-
-  },
-  notifIcon: {
-    padding: 5,
-    marginLeft: 10,
-    alignSelf: 'center',
-    backgroundColor: 'rgb(255, 0, 102)',
-    borderRadius: 25,
-
-  },
-  notifText: {
-    padding: 5,
-    marginLeft: "2%",
-    color: 'white',
-    fontSize: 15,
-
-  }
-
 
 })
