ba370300f3522a2595784dfc851740b65b06c46e
aez9 <aez9@calvin.edu>
2022-11-17 20:21:36 -0500

    Update notifications.js

--- screens/notifications.js
+++ screens/notifications.js
@@ -16,9 +16,7 @@ export default function Notifications() {
   const getNotifications = async () => {
     try {
       const response = await fetch('https://be-a-ruby.herokuapp.com/notifications') //fetch the database notifications
-      // console.log(response);
       const json = await response.json();
-      // console.log(json);
       setData(json);
     } catch (error) {
       console.log(error)
@@ -44,7 +42,6 @@ export default function Notifications() {
           data={data}
           keyExtractor={(item) => `${item.name}:${item.quantity}`} // extracting info from json fetch
           renderItem={({ item }) => (
-            // <Text>{item.iname} {item.quantity}</Text> //test that necessary information was reaching notification component
             <NotificationItem item={item.iname} amount={item.quantity}></NotificationItem>
           )}>
         </FlatList>
